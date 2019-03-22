4f4820c6b10730c56e5d66eb16a0dae12d7a8b52
Maximilian Mikus <maximilianmikus@gmail.com>
2019-03-22 12:40:22 +0100

    Move to vue-cli setup.

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import PgQuote from './pg-quote.vue'
-
-export { PgQuote }
-
-export default PgQuote
--- src/pg-quote.vue
+++ src/pg-quote.vue
@@ -24,29 +24,3 @@
     name: 'PgQuote'
   };
 </script>
-<style lang="scss" scoped>
-.paragraph--quote {
-  .content {
-    padding: 1em 2em;
-    border-left: 3px solid grey;
-  }
-
-  .title {
-    h1,
-    h2,
-    h3,
-    h4,
-    h5 {
-      margin-top: 0;
-    }
-  }
-
-  .author {
-    font-weight: bold;
-
-    &::before {
-      content: '\2014\00a0';
-    }
-  }
-}
-</style>
